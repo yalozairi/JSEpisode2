1e6013fa627e7e028b36a0a195fddd9e9635efc1
Yousif AlOzairi <yoyo012@icloud.com>
2020-06-22 01:19:03 +0300

    horrible, convoluted, messy, and temporary  solution -but a solution nonetheless. (it got really late and tiresome.. hopefully will be able to optimize this tomorrow :)

--- pairs.js
+++ pairs.js
@@ -1,44 +1,87 @@
 /**************************************************************
-* pairs(names):
-*
-* - It accepts an array of names:
-*       pairs(['Asis', 'Hamsa', 'Fawas', 'Mishmish'])
-*
-* - It returns a randomized pairing of names:
-*       [['Mishmish', 'Asis'], ['Fawas', 'Hamsa']]
-*
-* - It can handle an odd number of names:
-*       pairs(['Asis', 'Hamsa', 'Fawas', 'Mishmish', 'Hussein'])
-*       returns [['Mishmish', 'Fawas'], ['Asis', 'Hussein'], ['Hamsa']]
-*
-* - It returns an empty array if it gets passed an empty array:
-*       pairs([]) returns []
-*
-* - It returns an empty array if it gets passed nothing:
-*       pairs() returns []
-****************************************************************/
+ * pairs(names):
+ *
+ * - It accepts an array of names:
+ *       pairs(['Asis', 'Hamsa', 'Fawas', 'Mishmish'])
+ *
+ * - It returns a randomized pairing of names:
+ *       [['Mishmish', 'Asis'], ['Fawas', 'Hamsa']]
+ *
+ * - It can handle an odd number of names:
+ *       pairs(['Asis', 'Hamsa', 'Fawas', 'Mishmish', 'Hussein'])
+ *       returns [['Mishmish', 'Fawas'], ['Asis', 'Hussein'], ['Hamsa']]
+ *
+ * - It returns an empty array if it gets passed an empty array:
+ *       pairs([]) returns []
+ *
+ * - It returns an empty array if it gets passed nothing:
+ *       pairs() returns []
+ ****************************************************************/
 function pairs(names) {
-  // Your code goes here
+  let randomizedPairs = [];
+  if (names === []) {
+    return randomizedPairs;
+  }
+  if (names === undefined) {
+    return randomizedPairs;
+  }
+
+  while (names.length !== 0) {
+    if (names !== []) {
+      randomizedPairs.push([names.getRandom(), names.getRandom()]);
+    }
+  }
+  if (names === []) {
+    return randomizedPairs;
+  } else if (
+    names !== [] &&
+    randomizedPairs[randomizedPairs.length - 1] === undefined
+  ) {
+    return randomizedPairs;
+  } else if (randomizedPairs[randomizedPairs.length - 1][1] === undefined) {
+    randomizedPairs[randomizedPairs.length - 1].pop();
+  }
+  return randomizedPairs;
 }
+
+//   let randomizedNames = names.getRandom();
+//   let randomizedPairs = [];
+
+//   for (let i = 0; i < randomizedNames.length; i++) {
+
+//     if (i < randomizedNames.length - 1){
+//     randomizedPairs.push([randomizedNames[i], randomizedNames[i+1])
+//   } else {
+//     randomizedPairs.push([randomizedNames[i]])
+//   }
+// }
+// return randomizedPairs;
+
+// let randomNames = shuffle(names);
+
+// return (
+//   randomPair1.slice(0, randomNames / 2) &&
+//   randomPair2.slice(randomNames / 2, randomNames.length)
+// );
 
 module.exports = pairs;
 
 /**********************************************
-* READ ME!!!!
-*
-* We've included this handy method for you.
-* It will remove a random element from an array
-* and return it to you.
-*
-* Example Usage:
-*
-* let numbers = [1, 2, 3, 4];
-* let random = numbers.getRandom();  // randomly returns something from the array. e.g. 3
-* console.log(random); // 3 (the random element)
-* console.log(numbers);  // [1, 2, 4] (missing the random element)
-************************************************/
+ * READ ME!!!!
+ *
+ * We've included this handy method for you.
+ * It will remove a random element from an array
+ * and return it to you.
+ *
+ * Example Usage:
+ *
+ * let numbers = [1, 2, 3, 4];
+ * let random = numbers.getRandom();  // randomly returns something from the array. e.g. 3
+ * console.log(random); // 3 (the random element)
+ * console.log(numbers);  // [1, 2, 4] (missing the random element)
+ ************************************************/
 Array.prototype.getRandom = function () {
-  return this.splice(Math.floor(Math.random()*this.length), 1)[0];
-}
+  return this.splice(Math.floor(Math.random() * this.length), 1)[0];
+};
 
-console.log(pairs(['Asis', 'Hamsa', 'Fawas', 'Mishmish', 'Hussein', 'Lailz', 'Mr Potato']));
+console.log(pairs([]));
